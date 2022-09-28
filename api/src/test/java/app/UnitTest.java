2efff14f9fbb5380276ef38ae8029738332975a4
NRSS <nss20@aluno.ifal.edu.br>
2022-09-28 22:21:27 +0000

    Adicionando os testes da operação de soma.

--- api/src/test/java/app/UnitTest.java
+++ api/src/test/java/app/UnitTest.java
@@ -8,6 +8,6 @@ public class UnitTest {
   @Test
   public void welcome() {
     Controller controller = new Controller();
-    assertEquals("Welcome to Jooby!", controller.sayHi());
+    assertEquals("Bem-vindo(a) a calculadora API || Atualmente recebendo comandos...", controller.sayHi());
   }
 }
